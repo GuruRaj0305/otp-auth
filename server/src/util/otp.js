d3849528d77837859be49dc2bdeb41d9086d8261
gururaj0305 <gururajhr0305l@gmail.com>
2026-05-16 13:56:54 +0530

    opt generation validations

---
+++ server/src/util/otp.js
@@ -0,0 +1,41 @@
+const OTP_LENGTH = Number(process.env.OTP_LENGTH) || 6;
+
+const generateOtp = () => {
+    let otp = "";
+    for (let i = 0; i < OTP_LENGTH; i++) { // this for loop beceuse to generate dymanic lentgh 
+        otp += Math.floor(Math.random() * 10);
+    }
+
+    return otp;
+};
+
+// if opt all digits are same then it is weak otp
+const isWeakOtp = (otp) => {
+  if (!otp) return true;
+
+  //  000000, ...
+  if (/^(\d)\1+$/.test(otp)) return true;
+
+  return false;
+};
+
+const generateAndValidateOtp = () => {
+
+    const maxGenerationAttempts = 5;
+    let attempts = 0;
+    let otp;
+
+    do {
+        otp = generateOtp();
+        attempts++;
+        
+    } while (isWeakOtp(otp) && attempts < maxGenerationAttempts);
+
+    return otp;
+};
+
+
+
+module.exports = {
+    generateAndValidateOtp,
+};  
